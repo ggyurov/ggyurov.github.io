04fc3367c58599063da997be9aca4fd1652cf741
Georgi Gyurov <georgi.gyurov@modacom.com>
2015-09-09 12:28:16 +0300

    Contact form beta test

--- mailer.php
+++ mailer.php
@@ -37,17 +37,17 @@
         // Send the email.
         if (mail($recipient, $subject, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
-            http_response_code(200);
+            console.log(http_response_code(200));
             echo "Thank You! Your message has been sent.";
         } else {
             // Set a 500 (internal server error) response code.
-            http_response_code(500);
+            console.log(http_response_code(500));
             echo "Oops! Something went wrong and we couldn't send your message.";
         }
 
     } else {
         // Not a POST request, set a 403 (forbidden) response code.
-        http_response_code(403);
+        console.log(http_response_code(403));
         echo "There was a problem with your submission, please try again.";
     }
 
